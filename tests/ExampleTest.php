1e2d2df6948a958f526a8febd984c2c91f97c691
Peter Geiger <mail@geigerpeter.hu>
2024-01-21 19:27:42 +0100

    test: Test added to supporting development

---
+++ tests/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    public function testSuccessExample(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testFailExample() : void
+    {
+        $this->assertTrue(false);
+    }
+
+    public function testErrorExample() : void
+    {
+        throw new \Exception('Error example');
+    }
+
+}
